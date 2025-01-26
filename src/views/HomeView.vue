40248116853c95381d065f6fa0bcb2946a8792cc
zimu <234047485@qq.com>
2025-01-26 11:50:51 +0800

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -23,7 +23,7 @@
     </div>
 
     <PokerTable />
-    <Actions :actions="actions" />
+    <Actions :actions="game.actions" />
   </div>
 </template>
 
@@ -38,50 +38,85 @@ export default {
   },
   data() {
     return {
-      actions: [
-        {
-          player: 'player1',
-          action: 'POST_SMALL_BLIND',
-          amount: 1,
-          message: 'Posting small blind.',
-          analysis: 'Player1决定下小盲注',
+      game: {
+        id: '987654321',
+        table: {
+          players: 2,
+          smallBlind: 1,
+          bigBlind: 2,
         },
-        {
-          player: 'player2',
-          action: 'POST_BIG_BLIND',
-          amount: 2,
-          message: 'Posting big blind.',
-          analysis: 'Player2决定下大盲注',
+        players: [
+          {
+            id: 'player1',
+            name: 'OpenAI o1-mini',
+            chips: 150,
+            chipsThisRound: 50,
+            status: 'ACTIVE',
+            holeCards: [0, 1],
+          },
+          {
+            id: 'player2',
+            name: 'DeepSeek R1',
+            chips: 200,
+            chipsThisRound: 0,
+            status: 'ACTIVE',
+            holeCards: [2, 3],
+          },
+        ],
+        dealer: 'player1',
+        communityCards: {
+          flop: [4, 5, 6],
+          turn: null,
+          river: null,
         },
-        {
-          player: 'player1',
-          action: 'RAISE',
-          amount: 5,
-          message: 'Raising to 6.',
-          analysis: 'Player1决定加注',
-        },
-        {
-          player: 'player2',
-          action: 'CALL',
-          amount: 4,
-          message: 'Calling your raise.',
-          analysis: 'Player2决定跟注',
-        },
-        {
-          player: 'player2',
-          action: 'CHECK',
-          amount: 0,
-          message: 'Checking on the flop.',
-          analysis: 'Player2决定过牌',
-        },
-        {
-          player: 'player1',
-          action: 'BET',
-          amount: 6,
-          message: 'Continuing the aggression.',
-          analysis: 'Player1决定继续加注',
-        },
-      ],
+        pot: 30,
+        currentRound: 'FLOP',
+        currentPlayerTurn: 'player2',
+        actions: [
+          {
+            player: 'player1',
+            action: 'POST_SMALL_BLIND',
+            amount: 1,
+            message: 'Posting small blind.',
+            analysis: 'player1 下小盲',
+          },
+          {
+            player: 'player2',
+            action: 'POST_BIG_BLIND',
+            amount: 2,
+            message: 'Posting big blind.',
+            analysis: 'player2 下大盲',
+          },
+          {
+            player: 'player1',
+            action: 'RAISE',
+            amount: 5,
+            message: 'Raising to 6.',
+            analysis: 'player1 加注',
+          },
+          {
+            player: 'player2',
+            action: 'CALL',
+            amount: 4,
+            message: 'Calling your raise.',
+            analysis: 'player2 跟注',
+          },
+          {
+            player: 'player2',
+            action: 'CHECK',
+            amount: 0,
+            message: 'Checking on the flop.',
+            analysis: 'player2 看牌',
+          },
+          {
+            player: 'player1',
+            action: 'BET',
+            amount: 6,
+            message: 'Continuing the aggression.',
+            analysis: 'player1 继续加注',
+          },
+        ],
+      },
     };
   },
 };
