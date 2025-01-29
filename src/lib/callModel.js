c057d5bc198f5475be4046b26b72c09e4194e1f1
zimu <234047485@qq.com>
2025-01-29 19:31:37 +0800

    Update callModel.js

--- src/lib/callModel.js
+++ src/lib/callModel.js
@@ -2,20 +2,26 @@ import axios from 'axios';
 import describeGame from './describeGame';
 
 async function callModel(game) {
-  const payload = describeGame(game);
+  const describe = describeGame(game);
 
   // form encode
   const formData = new FormData();
-  formData.append('input', payload);
+  formData.append('input', describe);
   formData.append('model', game.players.find((player) => player.id === game.currentPlayerTurn).model);
   const { data } = await axios.post('/api/action.php', formData);
   if (data.code !== 0) {
-    throw new Error(data.message);
+    return {
+      player: game.currentPlayerTurn,
+      action: 'FOLD',
+      amount: 0,
+      message: '由于模型返回出错，被系统判定为弃牌',
+      analysis: '',
+    };
   }
   const modelAction = data.data;
 
   return {
-    player: payload.currentPlayerTurn,
+    player: game.currentPlayerTurn,
     action: modelAction.action,
     amount: modelAction.amount,
     message: modelAction.message,
